097ed8458576239697b115764a4b2c5ffd97bb4f
Kat Brandt <joyfulkat11@gmail.com>
2022-08-05 13:38:42 -0600

    Adds test for book show page.

--- spec/requests/api/v1/books_request_spec.rb
+++ spec/requests/api/v1/books_request_spec.rb
@@ -32,4 +32,32 @@ describe "Books API" do
       expect(book[:number_sold]).to be_an Integer
     end
   end
+
+  it 'can get one book by its id' do
+    id = create(:book).id
+
+    get "/api/v1/books/#{id}"
+
+    expect(response).to be_successful
+
+    book = JSON.parse(response.body, symbolize_names: true)
+
+    expect(book).to have_key :id
+    expect(book[:id]).to be_an Integer
+
+    expect(book).to have_key :title
+    expect(book[:title]).to be_a String
+
+    expect(book).to have_key :author
+    expect(book[:author]).to be_a String
+
+    expect(book).to have_key :genre
+    expect(book[:genre]).to be_a String
+
+    expect(book).to have_key :summary
+    expect(book[:summary]).to be_a String
+
+    expect(book).to have_key :number_sold
+    expect(book[:number_sold]).to be_an Integer
+  end
 end
